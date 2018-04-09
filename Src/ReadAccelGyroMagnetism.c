597eb883b7b497058a5a1e0677b435a0b63ffb9d
Enoch Tsang <enoch.tsang@calgaryscientific.com>
2018-04-09 12:10:54 -0600

    format

--- Src/ReadAccelGyroMagnetism.c
+++ Src/ReadAccelGyroMagnetism.c
@@ -20,8 +20,8 @@ static const uint8_t READZ_HIGH_CMD = 0x2D;
 
 uint8_t address, dataIn;
 uint8_t accelXData[2], accelYData[2], accelZData[2];
-uint16_t accelX,accelY,accelZ;
-uint8_t gyroX,gyroY,gyroZ;
+uint16_t accelX, accelY, accelZ;
+uint8_t gyroX, gyroY, gyroZ;
 uint8_t magnetX, magnetY, magnetZ;
 
 void readAccelGyroMagnetismTask(void const* arg)
@@ -41,7 +41,7 @@ void readAccelGyroMagnetismTask(void const* arg)
 
     HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(&hspi1, &RESET_CMD, CMD_SIZE, CMD_TIMEOUT);
-    osDelay(3); 
+    osDelay(3);
     HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
 
 
@@ -49,39 +49,39 @@ void readAccelGyroMagnetismTask(void const* arg)
     {
         osDelayUntil(&prevWakeTime, READ_ACCEL_GYRO_MAGNETISM);
 
-       //    // Read register to return higher bit data from z-axis
-       // // address = 0x80|0x2D;
-       //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
-       //  HAL_SPI_Transmit(&hspi1,0x20,1,500);
-       //  osDelay(2);
-       //  //address = 0x00;
-       //  //HAL_SPI_Receive(&hspi1,&accelZ,1,500);
-       //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
+        //    // Read register to return higher bit data from z-axis
+        // // address = 0x80|0x2D;
+        //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
+        //  HAL_SPI_Transmit(&hspi1,0x20,1,500);
+        //  osDelay(2);
+        //  //address = 0x00;
+        //  //HAL_SPI_Receive(&hspi1,&accelZ,1,500);
+        //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
 
-       //  //accelZ = *hspi1.pRxBuffPtr;
-       //  //accelZ = address;
+        //  //accelZ = *hspi1.pRxBuffPtr;
+        //  //accelZ = address;
 
-       //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
-       //  HAL_SPI_Transmit(&hspi1, 0x67, 1, 500);
-       //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
+        //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
+        //  HAL_SPI_Transmit(&hspi1, 0x67, 1, 500);
+        //  HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
 
-       //  osDelay(2); 
+        //  osDelay(2);
 
         //HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
         //HAL_SPI_Transmit(&hspi1, &address, 1, 500);
 
 
-       /* Check XLDA in STATUS_REG (Accelerometer ready bit)
-        * Reading OUTX_XL/../ clears XLDA, wait for first sample
-        * Read again, discard data
-        */
+        /* Check XLDA in STATUS_REG (Accelerometer ready bit)
+         * Reading OUTX_XL/../ clears XLDA, wait for first sample
+         * Read again, discard data
+         */
 
         //address = 0x20;
         //HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
         //HAL_SPI_Transmit(&hspi1, &address, CMD_SIZE, CMD_TIMEOUT);
         //HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
 
-        //osDelay(2); 
+        //osDelay(2);
 
         //HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_RESET);
         //HAL_SPI_Transmit(&hspi1, &READZ_CMD, CMD_SIZE, CMD_TIMEOUT);
@@ -151,7 +151,7 @@ void readAccelGyroMagnetismTask(void const* arg)
         /* Average the values */
         accelX /= 3;
         accelY /= 3;
-        accelZ /= 3; 
+        accelZ /= 3;
 
         HAL_GPIO_WritePin(XL_CS_GPIO_Port, XL_CS_Pin, GPIO_PIN_SET);
 
@@ -159,13 +159,13 @@ void readAccelGyroMagnetismTask(void const* arg)
         osMutexWait(data->mutex_, 0);
         data->accelX_ = accelX;
         data->accelY_ = accelY;
-      	data->accelZ_ = accelZ;
+        data->accelZ_ = accelZ;
         // data->gyroX_ = gyroX;
         // data->gyroY_ = gyroY;
         // data->gyroZ_ = gyroZ;
         // data->magnetoX_ = magnetX;
         // data->magnetoY_ = magnetY;
         // data->magnetoZ_ = magnetZ;
-      	osMutexRelease(data->mutex_);
+        osMutexRelease(data->mutex_);
     }
 }
