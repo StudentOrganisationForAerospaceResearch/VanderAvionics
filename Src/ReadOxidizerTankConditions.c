bfd5690ca871a7699e3b18bb46a7af77d0949fde
Natalia <natalia.nzp@gmail.com>
2018-05-15 19:27:56 -0600

    Cleaned up the code. ADC accuracy still needs to be improved and resistor values need to be changed

--- Src/ReadOxidizerTankConditions.c
+++ Src/ReadOxidizerTankConditions.c
@@ -7,41 +7,53 @@
 
 #include "Data.h"
 
-static int READ_OXIDIZER_TANK_CONDITIONS_PERIOD = 1000;
+static int READ_OXIDIZER_TANK_CONDITIONS_PERIOD = 20;	// Sampling delay set to 50 Hz to match high frequency logging
+
+static const int ADC_POLL_TIMEOUT = 150;
 
 void readOxidizerTankConditionsTask(void const* arg)
 {
     OxidizerTankConditionsData* data = (OxidizerTankConditionsData*) arg;
     uint32_t prevWakeTime = osKernelSysTick();
 
-    HAL_ADC_Start(&hadc1);
-    double lowPressure = 0;
+    uint16_t adcRead = 0;	// Stores a 12 but value
+    double vo = 0;	// The voltage across R2
+    double vi = 0;	// The voltage across R1 + R2 in series
+    double tankPressure = 0;
+
+    // Resistor values in kOhms
+    int R1 = 60; //100
+    int R2 = 165; //133
+
+    HAL_ADC_Start(&hadc1);	// Start the ADC peripheral
+    osDelay(5);
 
     for (;;)
     {
         osDelayUntil(&prevWakeTime, READ_OXIDIZER_TANK_CONDITIONS_PERIOD);
 
-        if (HAL_ADC_PollForConversion(&hadc1, 1000000) == HAL_OK)
+        if (HAL_ADC_PollForConversion(&hadc1, ADC_POLL_TIMEOUT ) == HAL_OK)
         {
-            lowPressure = HAL_ADC_GetValue(&hadc1);
+            adcRead = HAL_ADC_GetValue(&hadc1);
         }
 
-        // Vin varies between 0.5V-4.5V, but the board requires a voltage of 3.3V or less.
-        // R1 = 60kOhm, R2 = 166kOhm -> Change resistors so max value is at 3V?
-        // Vo = R2/(R1+R2)*Vin, and Vo varies between 0.367V-3.3V
+        // vi to voltage divider varies between 0.5V-4.5V, but the board requires a voltage less than 3.3V.
+        // After the voltage divider, the voltage varies between 0.285V-2.57V
+        vo = 3.3 / pow(2, 12) * adcRead;
 
-        lowPressure = 3.3 / pow(2, 12) * lowPressure;
+        vi = (R2 + R1) / R2 * vo;	// Calculate the original voltage before the voltage divider.
 
-        // Vin = (R1+R2)/R2*Vo to get the original value.
-        lowPressure = (166 + 60) / 166 * lowPressure;
+        // The low pressure sensor is ratiometric. The pressure is 0 psi when the voltage is 0.5V, and is 300
+        // psi when the voltage is 4.5V. The equations is derived from this information.
+        tankPressure = (vi - 0.5) * 300 / 4;	// Tank pressure in psi
+        tankPressure = tankPressure * 1000; 	// Multiply by 1000 to keep decimal places
 
         if (osMutexWait(data->mutex_, 0) != osOK)
         {
             continue;
         }
 
-        lowPressure *= 1000; // Multiply by 1000 to keep decimal places - Final value in mV
-        data->pressure_ = (int32_t) lowPressure;
+        data->pressure_ = (int32_t) tankPressure;
         osMutexRelease(data->mutex_);
     }
 }
