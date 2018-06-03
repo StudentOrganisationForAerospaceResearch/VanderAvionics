da30db84b12f3d3dc50674328bde0dae69aa6236
Natalia <natalia.nzp@gmail.com>
2018-06-03 01:22:37 -0600

    Changed to individual transmit/receive calls for magnetometer - still doesn't work

--- Src/ReadAccelGyroMagnetism.c
+++ Src/ReadAccelGyroMagnetism.c
@@ -6,14 +6,14 @@
 
 #include "Data.h"
 
-static int READ_ACCEL_GYRO_MAGNETISM = 20;
+static int READ_ACCEL_GYRO_MAGNETISM = 200;
 
 static const int CMD_TIMEOUT = 150;
 
 #define READ_CMD_MASK 0x80
 #define WRITE_CMD_MASK 0x00
 #define ACCEL_GYRO_MASK 0x00
-#define MAGNETO_MASK 0x40
+#define MAGNETO_MASK 0x00
 
 // Register addresses
 #define G1_CTRL_REGISTER_ADDR 0x10 // CTRL_REG1_G (10h)
@@ -32,25 +32,32 @@ static const int CMD_TIMEOUT = 150;
 #define MAGENTO_SENSITIVITY 0.14 // Unit is mgauss/LSB
 
 // Full Commands
-static const uint8_t ACTIVATE_GYRO_ACCEL_CMD = G1_CTRL_REGISTER_ADDR | WRITE_CMD_MASK | ACCEL_GYRO_MASK;
+static const uint8_t ACTIVATE_GYRO_ACCEL_CMD = G1_CTRL_REGISTER_ADDR | WRITE_CMD_MASK;
 // 011 01 0 00 -> ODR 119, 500 DPS
 static const uint8_t ACTIVATE_GYRO_ACCEL_DATA = 0x68;	// ODR 119 has cutoff of 38Hz
 
-static const uint8_t SET_ACCEL_SCALE_CMD = XL6_CTRL_REGISTER_ADDR | WRITE_CMD_MASK | ACCEL_GYRO_MASK;
+static const uint8_t SET_ACCEL_SCALE_CMD = XL6_CTRL_REGISTER_ADDR | WRITE_CMD_MASK;
 // 011 01 0 00 -> ODR 119, +/- 16G
 static const uint8_t SET_ACCEL_SCALE_DATA = 0x68;
 
 static const uint8_t SET_MAGNETO_ODR_CMD = M1_CTRL_REGISTER_ADDR | WRITE_CMD_MASK;
-// 0 00 100 0 0 -> temp comp disabled, low performance mode, 10 ODR, fast ODR disabled, self-test disabled
-static const uint8_t SET_MAGNETO_ODR_DATA = 0x10;
+// 0 01 111 0 0 -> temp comp disabled, low performance mode, 10 ODR, fast ODR disabled, self-test disabled
+static const uint8_t SET_MAGNETO_ODR_DATA = 0x3C;
 
 static const uint8_t ACTIVATE_MAGNETO_CMD = M3_CTRL_REGISTER_ADDR | WRITE_CMD_MASK;
-// 1 0 0 00 0 00 -> I2C Disable, Low power mode disabled, SPI write enable, Continuous-conversion mode
+// 1 0 0 00 1 01 -> I2C Disable, Low power mode disabled, SPI write enable, Continuous-conversion mode
 static const uint8_t ACTIVATE_MAGNETO_DATA = 0x80;
 
 static const uint8_t READ_GYRO_X_G_LOW_CMD = GYRO_X_G_LOW_REGISTER_ADDR | READ_CMD_MASK | ACCEL_GYRO_MASK;
 static const uint8_t READ_ACCEL_X_LOW_CMD = ACCEL_X_LOW_REGISTER_ADDR | READ_CMD_MASK | ACCEL_GYRO_MASK;
 static const uint8_t READ_MAGNETO_X_LOW_CMD = MAGNETO_X_LOW_REGISTER_ADDR | READ_CMD_MASK | MAGNETO_MASK;
+static const uint8_t READ_MAGNETO_X_HIGH_CMD = 0x29 | READ_CMD_MASK | MAGNETO_MASK;
+static const uint8_t READ_MAGNETO_Y_LOW_CMD = 0x2A | READ_CMD_MASK | MAGNETO_MASK;
+static const uint8_t READ_MAGNETO_Y_HIGH_CMD = 0x2B | READ_CMD_MASK | MAGNETO_MASK;
+static const uint8_t READ_MAGNETO_Z_LOW_CMD = 0x2C | READ_CMD_MASK | MAGNETO_MASK;
+static const uint8_t READ_MAGNETO_Z_HIGH_CMD = 0x2D | READ_CMD_MASK | MAGNETO_MASK;
+
+
 // static const uint8_t READ_WHOAMI_CMD = WHOAMI_REGISTER_ADDR | READ_CMD_MASK | ACCEL_GYRO_MASK;
 // static const uint8_t READ_WHOAMIM_CMD = WHOAMIM_REGISTER_ADDR | READ_CMD_MASK | MAGNETO_MASK;
 
@@ -80,10 +87,10 @@ void readAccelGyroMagnetismTask(void const* arg)
     HAL_SPI_Transmit(&hspi1, &ACTIVATE_MAGNETO_DATA, 1, CMD_TIMEOUT);
     HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_SET);
 
-    HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_RESET);
-    HAL_SPI_Transmit(&hspi1, &SET_MAGNETO_ODR_CMD, 1, CMD_TIMEOUT);
-    HAL_SPI_Transmit(&hspi1, &SET_MAGNETO_ODR_DATA, 1, CMD_TIMEOUT);
-    HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_SET);
+    // HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_RESET);
+    // HAL_SPI_Transmit(&hspi1, &SET_MAGNETO_ODR_CMD, 1, CMD_TIMEOUT);
+    // HAL_SPI_Transmit(&hspi1, &SET_MAGNETO_ODR_DATA, 1, CMD_TIMEOUT);
+    // HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_SET);
 
     /* Read WHO AM I register for verification, should read 104. */
     // uint8_t whoami;
@@ -93,6 +100,7 @@ void readAccelGyroMagnetismTask(void const* arg)
     // HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_SET);
 
     uint8_t dataBuffer[6];
+
     int16_t accelX, accelY, accelZ;
     int16_t gyroX, gyroY, gyroZ;
     int16_t magnetoX, magnetoY, magnetoZ;
@@ -102,26 +110,40 @@ void readAccelGyroMagnetismTask(void const* arg)
         osDelayUntil(&prevWakeTime, READ_ACCEL_GYRO_MAGNETISM);
 
         //READ------------------------------------------------------
-        HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_RESET);
-        HAL_SPI_Transmit(&hspi1, &READ_GYRO_X_G_LOW_CMD, 1, CMD_TIMEOUT);
-        HAL_SPI_Receive(&hspi1, &dataBuffer[0], 6, CMD_TIMEOUT);
-        HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_SET);
-        gyroX = (dataBuffer[1] << 8) | (dataBuffer[0]);
-        gyroY = (dataBuffer[3] << 8) | (dataBuffer[2]);
-        gyroZ = (dataBuffer[5] << 8) | (dataBuffer[4]);
+        // HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_RESET);
+        // HAL_SPI_Transmit(&hspi1, &READ_GYRO_X_G_LOW_CMD, 1, CMD_TIMEOUT);
+        // HAL_SPI_Receive(&hspi1, &dataBuffer[0], 6, CMD_TIMEOUT);
+        // HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_SET);
+        // gyroX = (dataBuffer[1] << 8) | (dataBuffer[0]);
+        // gyroY = (dataBuffer[3] << 8) | (dataBuffer[2]);
+        // gyroZ = (dataBuffer[5] << 8) | (dataBuffer[4]);
 
-        HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_RESET);
-        HAL_SPI_Transmit(&hspi1, &READ_ACCEL_X_LOW_CMD, 1, CMD_TIMEOUT);
-        HAL_SPI_Receive(&hspi1, &dataBuffer[0], 6, CMD_TIMEOUT);
-        HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_SET);
-        accelX = (dataBuffer[1] << 8) | (dataBuffer[0]);
-        accelY = (dataBuffer[3] << 8) | (dataBuffer[2]);
-        accelZ = (dataBuffer[5] << 8) | (dataBuffer[4]);
+        // HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_RESET);
+        // HAL_SPI_Transmit(&hspi1, &READ_ACCEL_X_LOW_CMD, 1, CMD_TIMEOUT);
+        // HAL_SPI_Receive(&hspi1, &dataBuffer[0], 6, CMD_TIMEOUT);
+        // HAL_GPIO_WritePin(IMU_CS_GPIO_Port, IMU_CS_Pin, GPIO_PIN_SET);
+        // accelX = (dataBuffer[1] << 8) | (dataBuffer[0]);
+        // accelY = (dataBuffer[3] << 8) | (dataBuffer[2]);
+        // accelZ = (dataBuffer[5] << 8) | (dataBuffer[4]);
 
         HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_RESET);
-        HAL_SPI_Transmit(&hspi1, &READ_MAGNETO_X_LOW_CMD, 1, CMD_TIMEOUT);
-        HAL_SPI_Receive(&hspi1, &dataBuffer[0], 6, CMD_TIMEOUT);
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_X_LOW_CMD, &dataBuffer[0], 1, CMD_TIMEOUT);
+
+
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_X_HIGH_CMD, &dataBuffer[1], 1, CMD_TIMEOUT);
+
+
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_Y_LOW_CMD, &dataBuffer[2], 1, CMD_TIMEOUT);
+
+
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_Y_HIGH_CMD, &dataBuffer[3], 1, CMD_TIMEOUT);
+
+
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_Z_LOW_CMD, &dataBuffer[4], 1, CMD_TIMEOUT);
+
+        HAL_SPI_TransmitReceive(&hspi1, &READ_MAGNETO_Z_HIGH_CMD, &dataBuffer[5], 1, CMD_TIMEOUT);
         HAL_GPIO_WritePin(MAG_CS_GPIO_Port, MAG_CS_Pin, GPIO_PIN_SET);
+
         magnetoX = (dataBuffer[1] << 8) | (dataBuffer[0]);
         magnetoY = (dataBuffer[3] << 8) | (dataBuffer[2]);
         magnetoZ = (dataBuffer[5] << 8) | (dataBuffer[4]);
@@ -132,12 +154,12 @@ void readAccelGyroMagnetismTask(void const* arg)
             continue;
         }
 
-        data->accelX_ = accelX * ACCEL_SENSITIVITY; // mg
-        data->accelY_ = accelY * ACCEL_SENSITIVITY; // mg
-        data->accelZ_ = accelZ * ACCEL_SENSITIVITY; // mg
-        data->gyroX_ = gyroX * GYRO_SENSITIVITY; // mdps
-        data->gyroY_ = gyroY * GYRO_SENSITIVITY; // mdps
-        data->gyroZ_ = gyroZ * GYRO_SENSITIVITY; // mdps
+        // data->accelX_ = accelX * ACCEL_SENSITIVITY; // mg
+        // data->accelY_ = accelY * ACCEL_SENSITIVITY; // mg
+        // data->accelZ_ = accelZ * ACCEL_SENSITIVITY; // mg
+        // data->gyroX_ = gyroX * GYRO_SENSITIVITY; // mdps
+        // data->gyroY_ = gyroY * GYRO_SENSITIVITY; // mdps
+        // data->gyroZ_ = gyroZ * GYRO_SENSITIVITY; // mdps
         data->magnetoX_ = magnetoX * MAGENTO_SENSITIVITY; // mgauss
         data->magnetoY_ = magnetoY * MAGENTO_SENSITIVITY; // mgauss
         data->magnetoZ_ = magnetoZ * MAGENTO_SENSITIVITY; // mgauss
